8211120957842104189a2f0a97e76c43fff3669e
nischay Naman <nischaynaman2210@gmail.com>
2026-02-25 10:52:10 +0530

    nischay bhai

---
+++ PalindromeChecker.java
@@ -0,0 +1,11 @@
+public class PalindromeChecker {
+    private static final String APP_NAME = "Palindrome Checker App";
+    private static final String VERSION = "Version 1.0";
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println(VERSION);
+        System.out.println("==================================");        
+        System.out.println("Application is ready to check palindromes.");
+    }
+}
