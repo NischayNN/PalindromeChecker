690571469d1647dffde50efef81eb3268e13bf0c
nischay Naman <nischaynaman2210@gmail.com>
2026-03-04 21:56:47 +0530

    UC8: Palindrome using Singly Linked List

---
+++ UC8.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class UC8 {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle of linked list
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        // Compare halves
+        Node first = head;
+        Node second = prev;
+
+        while (second != null) {
+            if (first.data != second.data) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Node head = null;
+        Node tail = null;
+
+        // Convert string to linked list
+        for (char ch : input.toCharArray()) {
+            Node newNode = new Node(ch);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        if (isPalindrome(head)) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("Not a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
