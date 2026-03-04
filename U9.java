967b3f1701c8c388ff5d8ac57848cee953a3174f
nischay Naman <nischaynaman2210@gmail.com>
2026-03-04 22:10:34 +0530

    UC9: Palindrome using Recursion

---
+++ U9.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class U9 {
+
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input, 0, input.length() - 1)) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("Not a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
